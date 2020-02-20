d0c526509adf6c656f625fc5606a627fc3448018
Ben Cliff <bocliffwm@gmail.com>
2020-02-20 09:59:06 -0500

    Adding my code as comments

--- EmployeeSQL/EmployeesSQL.sql
+++ EmployeeSQL/EmployeesSQL.sql
@@ -1,1 +1,36 @@
-select * from titles
+--Creating Tables
+-- CREATE TABLE titles(
+-- 	emp_no INT,
+-- 	title VARCHAR(20),
+-- 	from_date VARCHAR(15),
+-- 	to_date VARCHAR(15));
+	
+-- CREATE TABLE salaries(
+-- 	emp_no INT,
+-- 	salary INT,
+-- 	from_date VARCHAR(15),
+-- 	to_date VARCHAR(15));
+	
+-- CREATE TABLE employees(
+-- 	emp_no INT,
+-- 	birth_date VARCHAR(15),
+-- 	first_name VARCHAR(20),
+-- 	last_name VARCHAR(20),
+-- 	gender VARCHAR(1),
+-- 	hire_date VARCHAR(20));	
+	
+-- CREATE TABLE departments(
+-- 	dept_no VARCHAR(4),
+-- 	dept_name VARCHAR(20));	
+	
+-- CREATE TABLE dept_emp(
+-- 	emp_no INT,
+-- 	dept_no VARCHAR(4),
+-- 	from_date VARCHAR(15),
+-- 	to_date VARCHAR(15));	
+	
+-- CREATE TABLE dept_manager(
+-- 	dept_no VARCHAR(4),
+-- 	emp_no INT,
+-- 	from_date VARCHAR(15),
+-- 	to_date VARCHAR(15));	
